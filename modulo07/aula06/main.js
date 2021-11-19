e8c446bcc8038386fe10ee5fd18dae121fd06f2b
renatomno <renatomno@hotmail.com>
2021-11-19 04:26:11 -0300

    add modulo07/aula06

---
+++ modulo07/aula06/main.js
@@ -0,0 +1,11 @@
+document.getElementById("nome").innerHTML = "Renato Oliveira"
+document.getElementById("idade").innerHTML = "25"
+document.getElementById("estado").innerHTML = "Rio Grande do Norte"
+document.getElementById("foto").setAttribute("src", "https://avatars.githubusercontent.com/u/51255373?v=4")
+
+document.getElementById("titulo").innerHTML = "Ilustração digital"
+document.getElementById("descricao").innerHTML = "Utilizando o software de sua escolha, é possível fazer pinturas e ilustrações! O limite é sua imaginação!"
+document.getElementById("consulta").innerHTML = "Para mais informações sobre:"
+document.getElementById("link").innerHTML = "Clique aqui"
+document.getElementById("link").setAttribute = ("href", "https://oque-e.com/o-que-e-ilustracao-digital/")
+document.getElementById("foto2").setAttribute("src", "https://ebac.art.br/upload/resize_cache/iblock/1df/770_500_2/photo-illustration.jpg")
