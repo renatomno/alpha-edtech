cec887fed335ecee9b2b8cecb7b1521deb330a29
Renato Oliveira <renatomno@hotmail.com>
2022-01-17 18:43:10 -0300

    add modulo07/aula25

---
+++ modulo07/aula25/main.js
@@ -0,0 +1,9 @@
+function arrayCreator(...theArgs) {
+    return theArgs
+}
+
+//Chamada da função 
+const arraySample = arrayCreator(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+//Impressão do resultado
+console.log(arraySample)
