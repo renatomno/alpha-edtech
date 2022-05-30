1b756f748bdc0b7d19a823a604a6b0afe65ca176
Renato Oliveira <xrenatomno@gmail.com>
2022-05-30 15:45:07 -0300

    add modulo11/aula08

---
+++ modulo11/aula08/src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+font-size: 40px;
+text-align: center;
+color: red;
+`;
+
+function App() {
+  return (
+    <Title> Olá, mundo!</Title>
+  )
+}
+
+export default App;
